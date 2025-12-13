ac038bab3145569f4dff2a213e47f6788eed4816
Lucas Muniz <lucas_olimuniz@hotmail.com>
2025-12-13 14:08:25 -0300

    fix: add CommonRules.In01

---
+++ src/Core/Data/Validation/Rules/CommonRules.cs
@@ -0,0 +1,10 @@
+﻿namespace FireAndSteel.Core.Data.Validation.Rules;
+
+public static class CommonRules
+{
+    public static bool NonEmpty(string? value) => !string.IsNullOrWhiteSpace(value);
+
+    public static bool In01(double value) => value >= 0.0 && value <= 1.0;
+    public static bool In01(float value)  => value >= 0.0f && value <= 1.0f;
+    public static bool In01(decimal value)=> value >= 0m && value <= 1m;
+}
